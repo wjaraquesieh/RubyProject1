96f8f2966219af48e79b30243976a355a0845849
Wadia Jara Quesieh <wjaraquesieh@academic.rrc.ca>
2024-10-30 16:50:36 -0500

    Brand controller with index view created

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :product_type
-  belongs_to :category
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+  has_many :product_tags
+  has_many :tags, through: :product_tags
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "brand/index"
   get "product/index"
   get "product/show"
   get "home/index"
