9f1733c87e38d368ead81c70fc6f008223d63e85
Wadia Jara Quesieh <wjaraquesieh@academic.rrc.ca>
2024-10-30 15:42:23 -0500

    Modification on product controller, index and show view

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,7 +1,19 @@
 class ProductController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  def search
+    @query = params[:query]
+    @type = params[:type]
+
+    @products = Product.where("name LIKE ?", "%#{@query}%")
+    @products = @products.where(type: @type) unless @type == "all"
+
+    render :index
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,48 @@ end
 #     description: Faker::Lorem.paragraph
 #   )
 # end
+
+require 'net/http'
+require 'json'
+
+# Clean the records
+ProductTag.delete_all
+ProductColor.delete_all
+Product.delete_all
+
+url = URI("http://makeup-api.herokuapp.com/api/v1/products.json")
+response = Net::HTTP.get(url)
+products = JSON.parse(response)
+
+products.each do |product_data|
+  # Find or create associated records
+  brand = Brand.find_or_create_by(description: product_data["brand"])
+  product_type = ProductType.find_or_create_by(description: product_data["product_type"])
+  category = Category.find_or_create_by(description: product_data["category"])
+
+  # Create Product
+  product = Product.create(
+    name: product_data["name"],
+    brand: brand,
+    product_type: product_type,
+    category: category,
+    price: product_data["price"].to_f,
+    currency: product_data["currency"],
+    image: product_data["image_link"],
+    link: product_data["product_link"],
+    description: product_data["description"],
+    rating: product_data["rating"].to_f
+  )
+
+  # Handle tags
+  product_data["tag_list"].each do |tag_name|
+    tag = Tag.find_or_create_by(tagName: tag_name)
+    ProductTag.create(product: product, tag: tag)
+  end
+
+  # Handle colors
+  product_data["product_colors"].each do |color_data|
+    color = Color.find_or_create_by(hex_value: color_data["hex_value"], colorName: color_data["colour_name"])
+    ProductColor.create(product: product, color: color)
+  end
+end
